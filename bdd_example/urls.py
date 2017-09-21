f94992da8dff734fb344a713aafad3bf775418a1
nunchuck <nicholas.ball@sainsburys.co.uk>
2017-09-21 17:11:38 +0100

    Added pipenv functionality, created URL mapping and views. Wrote the Behave test functions so that the login app can be tested.

--- bdd_example/urls.py
+++ bdd_example/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from login import views as login_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', login_views.login_root, name='login_root'),
+    url(r'^login/success/$', login_views.login_success, name='login_success'),
+    url(r'^login/fail/$', login_views.login_fail, name='login_fail')
 ]
